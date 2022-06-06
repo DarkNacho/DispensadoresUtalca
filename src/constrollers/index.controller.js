a0b5a623b65413e5561219afaf0f9bf4121f9ff8
DarkNacho <dark_nacho_xd@hotmail.cl>
2022-06-06 16:57:24 -0400

    Button On/Off + Led
    Disponible para dos botones y leds

--- src/constrollers/index.controller.js
+++ src/constrollers/index.controller.js
@@ -1,9 +1,12 @@
 const controller = {}
 const Gpio = require('onoff').Gpio
 
-var led = new Gpio(21, 'out')
+var led1 = new Gpio(21, 'out')
+var pushButton1 = new Gpio(20, 'in','both')
 
-var pushButton = new Gpio(20, 'in','both')
+
+var led2 = new Gpio(26, 'out')
+var pushButton2 = new Gpio(16, 'in','both')
 
 
 controller.index = (req, res) => {
@@ -11,30 +14,39 @@ controller.index = (req, res) => {
 }
 
 controller.n1000 = (req, res) => {
-	led.writeSync(1);
-    res.send('prender led')
+	led1.writeSync(1);
+    res.send('prender led 1')
 }
 
 controller.n2000 = (req, res) => {
-	led.writeSync(0);
-    res.send('apagar led')
+	led2.writeSync(1);
+    res.send('prenderled 2')
 }
 
 controller.avisar = (req, res) =>
 {
 	const { id } = req.params
 	//get led b id here then write HIGH
-	led.writeSync(1);
+	//led.writeSync(1);
     res.send('encender led en id: ' + id)
 	
 }
 
-pushButton.watch(function (err, value){
+pushButton1.watch(function (err, value){
 	if(err) {
 		console.error('hay un error', err)
 		return
 	}
-	led.writeSync(value)
+	led1.writeSync(value)
+	})
+	
+	
+pushButton2.watch(function (err, value){
+	if(err) {
+		console.error('hay un error', err)
+		return
+	}
+	led2.writeSync(value)
 	})
 
 module.exports = controller
