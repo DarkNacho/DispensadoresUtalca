d7d64b55dbea311327a5961b3456029abf313918
DarkNaho <dark_nacho_xd@hotmail.cl>
2022-06-06 17:08:13 +0000

    update

--- src/constrollers/index.controller.js
+++ src/constrollers/index.controller.js
@@ -1,15 +1,40 @@
 const controller = {}
+const Gpio = require('onoff').Gpio
+
+var led = new Gpio(21, 'out')
+
+var pushButton = new Gpio(20, 'in','both')
+
 
 controller.index = (req, res) => {
     res.send('la conexion ha sido correcta desde index.controller')
 }
 
 controller.n1000 = (req, res) => {
-    res.send('tu reporte del dispensador 1 se ha enviado')
+	led.writeSync(1);
+    res.send('prender led')
 }
 
 controller.n2000 = (req, res) => {
-    res.send('tu reporte del dispensador 2 se ha enviado')
+	led.writeSync(0);
+    res.send('apagar led')
 }
+
+controller.avisar = (req, res) =>
+{
+	const { id } = req.params
+	//get led b id here then write HIGH
+	led.writeSync(1);
+    res.send('encender led en id: ' + id)
+	
+}
+
+pushButton.watch(function (err, value){
+	if(err) {
+		console.error('hay un error', err)
+		return
+	}
+	led.writeSync(value)
+	})
 
 module.exports = controller
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -7,5 +7,6 @@ router.get('/', controller.index)
 
 router.get('/1000', controller.n1000)
 router.get('/2000', controller.n2000)
+router.get('/:id', controller.avisar)
 
 module.exports = router
